590bc4e265b5437efa4c8469c11a0d66cf5b7c4f
Chris Yuen <chris@kizzx2.com>
2014-12-11 00:02:35 +0800

    Time bug fix

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,6 @@ class Member < ActiveRecord::Base
   def yesterday_entries
     Entry.where(
       member_id: self.id,
-      created_at: 1.day.ago.beginning_of_day..1.day.ago.end_of_day)
+      created_at: 1.day.ago.beginning_of_day..Time.zone.now)
   end
 end
